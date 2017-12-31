afc3d0f5964083512f56eee8dd79a829380dcf3a
Alexis R <alexis.reymann@telecom-paristech.fr>
2017-12-31 12:37:39 +0100

    Adding my C++ template

---
+++ template.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <map>
+#include <deque>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define REP(i, n) FOR(i, 0, n)
+#define SORT(v) sort(v.begin(), v.end())
+#define SORTFUNC(v, func) sort(v.begin(), v.end(), func)
+#define PRINT(v) REP(i, v.size()) printf("%d ", v[i]); cout << endl
+#define SCI(n) int (n); scanf("%d", &(n))
+#define SCL(n) ll (n); scanf("%I64d", &(n))
+#define IN(n) scanf("%d", &(n))
+
+int main(){
+	int n;
+	char c;
+	cin >> n;
+	int dp[5010][5010];
+	memset(dp, 0, 5010*5010*sizeof(int));
+	//dp[i][j] = nombre de manières d'avoir le ième élément à l'indentation j
+	FOR(i, 1, n+1){
+		cin >> c;
+		
+	}
+}
